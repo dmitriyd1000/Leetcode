5eeddb8871495c81f4cb16d00af796e59f54605d
dmytro.dmytriiev <dmytro.dmytriiev@medeanalytics.com>
2025-05-08 15:38:59 -0700

    Riders changes

--- obj/Debug/net9.0/Algorithm.AssemblyInfo.cs
+++ obj/Debug/net9.0/Algorithm.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Algorithm")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5430e1721c4ec96e3d5db63cbe591a2158d7c380")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45e720aa5999d838b865c63d25ff1a92195331f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Algorithm")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Algorithm")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
