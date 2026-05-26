d8fa2c6c323bcc8e2664fa00cc5c7c0d1c8df3b4
dmitriyd1000 <dmitriyd1000@gmail.com>
2026-05-26 14:37:12 -0700

    Add TwoSumTask implementation and corresponding tests; update project to .NET 10.0

---
+++ TwoSum/TwoSumTask.cs
@@ -0,0 +1,20 @@
+﻿namespace Algorithm.TwoSum;
+
+public class TwoSumTask
+{
+    public (int, int) TwoSum(int[] nums, int target)
+    {
+        var dict = new Dictionary<int, int>();
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var diff = target - nums[i];
+            if (dict.TryGetValue(nums[i], out var value))
+            {
+                return (value, i);
+            }
+            // defence from repeatable
+            _ = dict.TryAdd(diff, i);
+        }
+        return (0,0);
+    }
+}
---
+++ TwoSum/TwoSumTaskTest.cs
@@ -0,0 +1,32 @@
+﻿using Shouldly;
+
+namespace Algorithm.TwoSum;
+
+[TestFixture]
+public class TwoSumTaskTest
+{
+    private TwoSumTask _target;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _target = new TwoSumTask();
+    }
+    
+    [TearDown]
+    public void TearDown()
+    {
+        
+    }
+    
+    [TestCase(new int[] {6,3,6,3,4,6,8 }, 7, ExpectedResult = new object[] {1, 4})]
+    public object[] TwoSumTaskTest1(int[] nums, int target)
+    {
+        // arrange
+
+        //act
+        var result = _target.TwoSum(nums, target);
+        // assert
+        return [result.Item1, result.Item2];
+    }
+}
